13bf80115af0081f7b456921bf84323de09fd6f9
omar alvarez <xxosurfer3xx@gmail.com>
2015-07-28 10:33:57 +0200

    Initial Commit

---
+++ InboxNotifier/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  InboxNotifier
+//
+//  Created by Luis Omar Alvarez Mures on 28/07/15.
+//  Copyright (c) 2015 Luis Omar Alvarez Mures. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
