7a5ef6d45d8a6cd8c56b5587e5406f0558b2a201
omar alvarez <xxosurfer3xx@gmail.com>
2015-07-29 09:51:48 +0200

    StatusBar icon functionality and custom icons implemented

--- InboxNotifier/AppDelegate.swift
+++ InboxNotifier/AppDelegate.swift
@@ -13,14 +13,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
     
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1) //Variable size
+    
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
         if let button = statusItem.button {
-            button.image = NSImage(named: "StatusBarButtonImage")
+            button.image = NSImage(named: "icon_grey_simple")
+            button.title = NSString(string: "222  ")
+            //button.setFrameSize(NSMakeSize(30, 20))
+            button.imagePosition = NSCellImagePosition(rawValue: 2)!
             button.action = Selector("printQuote:")
         }
+        
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Print Quote", action: Selector("printQuote:"), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separatorItem())
+        menu.addItem(NSMenuItem(title: "Quit Quotes", action: Selector("terminate:"), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
